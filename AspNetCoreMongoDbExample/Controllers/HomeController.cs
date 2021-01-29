e4ac3e18fb8c672322d38d9b32b0ad8e6deceb7b
Oğuzcan Genç <mail@oguzcangenc.com>
2021-01-29 21:51:47 +0300

    Rename solution

--- AspNetCoreMongoDbExample/Controllers/HomeController.cs
+++ AspNetCoreMongoDbExample/Controllers/HomeController.cs
@@ -1,24 +1,24 @@
-﻿using AspNetCoreMongoDbExample.Models;
+﻿using AspNetCoreMongoDbExample.Core;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using MongoDB.Bson;
-using MongoDB.Driver;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace AspNetCoreMongoDbExample.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IShipwrecksService _shipwrecks;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IShipwrecksService shipwrecksService)
         {
             _logger = logger;
+            _shipwrecks = shipwrecksService;
         }
 
         public async Task<IActionResult> Index()
         {
+            var model = _shipwrecks.GetShipwrecks();
             //var connect = new MongoClient("mongodb+srv://ouzdev:etj810c222@cluster0.zq4ai.mongodb.net/<dbname>?retryWrites=true&w=majority");
             //var database = connect.GetDatabase("sample_geospatial");
             //var collection = database.GetCollection<Shipwrecks>("shipwrecks");
@@ -27,54 +27,54 @@ namespace AspNetCoreMongoDbExample.Controllers
             return View(model);
         }
 
-        [HttpGet]
-        public IActionResult DeleteById(string id)
-        {
-            var connect = new MongoClient("mongodb+srv://ouzdev:etj810c222@cluster0.zq4ai.mongodb.net/<dbname>?retryWrites=true&w=majority");
-            var database = connect.GetDatabase("sample_geospatial");
-            var collection = database.GetCollection<BsonDocument>("shipwrecks");
-            var deleteFilter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(id));
-            var result = collection.DeleteOne(deleteFilter);
+        //[HttpGet]
+        //public IActionResult DeleteById(string id)
+        //{
+        //    var connect = new MongoClient("mongodb+srv://ouzdev:etj810c222@cluster0.zq4ai.mongodb.net/<dbname>?retryWrites=true&w=majority");
+        //    var database = connect.GetDatabase("sample_geospatial");
+        //    var collection = database.GetCollection<BsonDocument>("shipwrecks");
+        //    var deleteFilter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(id));
+        //    var result = collection.DeleteOne(deleteFilter);
 
-            return RedirectToAction("Index");
-        }
+        //    return RedirectToAction("Index");
+        //}
 
-        public IActionResult AddRecord()
-        {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult AddRecord(Shipwrecks model)
-        {
-            var connect = new MongoClient("mongodb+srv://ouzdev:etj810c222@cluster0.zq4ai.mongodb.net/<dbname>?retryWrites=true&w=majority");
-            var database = connect.GetDatabase("sample_geospatial");
-            var collection = database.GetCollection<BsonDocument>("shipwrecks");
-            var bsonDocument = model.ToBsonDocument();
+        //public IActionResult AddRecord()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public IActionResult AddRecord(Shipwrecks model)
+        //{
+        //    var connect = new MongoClient("mongodb+srv://ouzdev:etj810c222@cluster0.zq4ai.mongodb.net/<dbname>?retryWrites=true&w=majority");
+        //    var database = connect.GetDatabase("sample_geospatial");
+        //    var collection = database.GetCollection<BsonDocument>("shipwrecks");
+        //    var bsonDocument = model.ToBsonDocument();
 
-            collection.InsertOne(bsonDocument);
+        //    collection.InsertOne(bsonDocument);
 
-            return View();
-        }
-        [HttpGet]
-        public async Task<IActionResult> UpdateRecord(string id)
-        {
-            var connect = new MongoClient("mongodb+srv://ouzdev:etj810c222@cluster0.zq4ai.mongodb.net/<dbname>?retryWrites=true&w=majority");
-            var database = connect.GetDatabase("sample_geospatial");
-            var collection = database.GetCollection<Shipwrecks>("shipwrecks");
-            Shipwrecks model = await collection.Find(s => s.Id == id).FirstOrDefaultAsync();
+        //    return View();
+        //}
+        //[HttpGet]
+        //public async Task<IActionResult> UpdateRecord(string id)
+        //{
+        //    var connect = new MongoClient("mongodb+srv://ouzdev:etj810c222@cluster0.zq4ai.mongodb.net/<dbname>?retryWrites=true&w=majority");
+        //    var database = connect.GetDatabase("sample_geospatial");
+        //    var collection = database.GetCollection<Shipwrecks>("shipwrecks");
+        //    Shipwrecks model = await collection.Find(s => s.Id == id).FirstOrDefaultAsync();
 
-            return View(model);
-        }
-        [HttpPost]
-        public async Task<IActionResult> UpdateRecord(Shipwrecks model)
-        {
+        //    return View(model);
+        //}
+        //[HttpPost]
+        //public async Task<IActionResult> UpdateRecord(Shipwrecks model)
+        //{
 
-            var connect = new MongoClient("mongodb+srv://ouzdev:etj810c222@cluster0.zq4ai.mongodb.net/<dbname>?retryWrites=true&w=majority");
-            var database = connect.GetDatabase("sample_geospatial");
-            var collection = database.GetCollection<Shipwrecks>("shipwrecks");
-            await collection.ReplaceOne(s => s.Id == model.Id, model);
+        //    var connect = new MongoClient("mongodb+srv://ouzdev:etj810c222@cluster0.zq4ai.mongodb.net/<dbname>?retryWrites=true&w=majority");
+        //    var database = connect.GetDatabase("sample_geospatial");
+        //    var collection = database.GetCollection<Shipwrecks>("shipwrecks");
+        //    await collection.ReplaceOne(s => s.Id == model.Id, model);
 
-            return RedirectToView("Index");
-        }
+        //    return RedirectToView("Index");
+        //}
     }
 }
